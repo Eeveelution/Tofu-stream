afdee8c0bcb6d1371343f16d7aee3dc3f4a7e495
Eevee <developerbrzoza@gmail.com>
2022-01-08 22:18:32 +0100

    Use Swagger and Controllers

--- Tofu.Stream/Startup.cs
+++ Tofu.Stream/Startup.cs
@@ -7,22 +7,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Tofu.Stream {
     public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services) {}
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                                 Title = "Tofu.Stream", Version = "v1"
+                });
+            });
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tofu.Stream Ingame Web Routes"));
             }
 
+            app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); }); });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
